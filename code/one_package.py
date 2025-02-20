4fb415719381c7c0d494b6777ea96a76f82726b0
ntbamber <ntbamber@syr.edu>
2025-02-20 13:20:06 -0500

    one_package commit

--- code/one_package.py
+++ code/one_package.py
@@ -6,3 +6,18 @@ Calculate the total package size and display that.
 
 see one_package.png for a screenshot
 '''
+import packaging
+import streamlit as st
+st.title("Process One Package 📦")
+package_input = st.text_input("Enter package information:")
+
+if package_input:
+    package = packaging.parse_packaging(package_input)
+    unit_name = None
+    for item in package:
+        for key, value in item.items():
+            if unit_name is None:
+                unit_name = key.capitalize()
+            st.write(f"{key.capitalize()} ➡️ {value}")
+    total_units = packaging.calc_total_units(package)
+    st.write(f"Total Size: {total_units} {unit_name} 📦")

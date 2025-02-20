95dd89621a58d0e90b27be8c7418a8558d179cf2
ntbamber <ntbamber@syr.edu>
2025-02-20 14:02:10 -0500

    process_files commit

--- code/process_files.py
+++ code/process_files.py
@@ -14,19 +14,15 @@ import json
 import os
 
 st.title("Process Multiple Packages 📦")
-
 uploaded_files = st.file_uploader("Upload files containing package information", type="txt", accept_multiple_files=True)
-
 total_files_processed = 0
 total_lines_processed = 0
-
 if uploaded_files:
     for uploaded_file in uploaded_files:
         file_content = uploaded_file.getvalue().decode("utf-8")
         json_filename = uploaded_file.name.replace(".txt", ".json")
         parsed_packages = []
         lines_in_file = 0
-
         for package_line in file_content.splitlines():
             package_line = package_line.strip()
             if package_line:
@@ -35,14 +31,10 @@ if uploaded_files:
                 base_unit = packaging.get_unit(parsed_pkg)
                 parsed_packages.append(parsed_pkg)
                 lines_in_file += 1
-                st.info(f"{package_line} ➡️ Total 📦 Size: {total_items} {base_unit}")
-
         with open(f"./data/{json_filename}", "w") as json_file:
             json.dump(parsed_packages, json_file, indent=4)
-
         st.success(f"{len(parsed_packages)} packages saved to {json_filename}", icon="💾")
         total_files_processed += 1
         total_lines_processed += lines_in_file
-
     st.write(f"Total files processed: {total_files_processed}")
     st.write(f"Total lines processed: {total_lines_processed}")
